926e6ac0de36171a42548f121aa4e2fc32b13def
ilyamoskalev <ilyamoskalev@mail.ru>
2017-10-06 19:35:50 +0300

    rus messages

--- src/main/java/application/utils/Messages.java
+++ src/main/java/application/utils/Messages.java
@@ -1,13 +1,13 @@
 package application.utils;
 
 public class Messages {
-    public static final String NOT_AUTHORIZE = "User not authorize";
-    public static final String AUTHORIZED = "User already authorized";
-    public static final String EXISTS = "Login or Email already exist";
-    public static final String WRONG_LOGIN_PASSWORD = "Wrong login or password";
-    public static final String WRONG_PASSWORD = "Wrong password";
-    public static final String LOGIN_EXISTS = "Login already exist";
-    public static final String EMAIL_EXISTS = "Email already exist";
-    public static final String SUCCESS = "Successful";
-    public static final String BAD_COOKIE = "Bad cookie";
+    public static final String NOT_AUTHORIZE = "Это фиаско, братан!";
+    public static final String AUTHORIZED = "Пользователь авторизирован";
+    public static final String EXISTS = "Логин или Email уже существует";
+    public static final String WRONG_LOGIN_PASSWORD = "Неправильный логин или пароль";
+    public static final String WRONG_PASSWORD = "Неправильный пароль";
+    public static final String LOGIN_EXISTS = "Логин уже существует";
+    public static final String EMAIL_EXISTS = "Email уже существует";
+    public static final String SUCCESS = "Успешно";
+    public static final String BAD_COOKIE = "Плохие печеньки";
 }
