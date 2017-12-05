fef7ed314fec1c195f198d31bed48783d1b312f6
ilyamoskalev <ilyamoskalev@mail.ru>
2017-12-05 19:55:48 +0300

    refactor

--- src/main/java/application/mechanic/Config.java
+++ src/main/java/application/mechanic/Config.java
@@ -10,17 +10,4 @@ public class Config {
     public static final String STEP_1_5 = "secondRecording";
     public static final String STEP_2 = "Listening";
     public static final String FINAL_STEP = "Result";
-//    public static final String STEP_4 = "record";
-//    public static final String STEP_5 = "waiting for answer";
-//    public static final String STEP_6 = "answer";
-//    public static final String STEP_7 = "finish game";
-//    public static final String STEP_8 = "";
-//    public static final String STEP_9 = "";
-//    public static final String STEP_10 = "";
-//    public static final String STEP_11 = "";
-//    public static final String STEP_12 = "";
-//    public static final String STEP_13 = "";
-//    public static final String STEP_14 = "";
-//    public static final String STEP_15 = "";
-//    public static final String STEP_16 = "";
 }
--- src/main/java/application/mechanic/GameMechanics.java
+++ src/main/java/application/mechanic/GameMechanics.java
@@ -2,7 +2,6 @@ package application.mechanic;
 
 import application.mechanic.internal.ClientSnapService;
 import application.mechanic.internal.GameSessionService;
-import application.mechanic.internal.ServerSnapService;
 import application.mechanic.snapshots.ClientSnap;
 import application.models.User;
 import application.services.AccountService;
@@ -24,8 +23,6 @@ public class GameMechanics {
     @NotNull
     private final ClientSnapService clientSnapshotsService;
     @NotNull
-    private final ServerSnapService serverSnapshotService;
-    @NotNull
     private final GameSessionService gameSessionService;
     @NotNull
     private final AccountService accountService;
@@ -40,12 +37,10 @@ public class GameMechanics {
     private final Queue<Runnable> tasks = new ConcurrentLinkedQueue<>();
 
     public GameMechanics(@NotNull ClientSnapService clientSnapshotsService,
-                         @NotNull ServerSnapService serverSnapshotService,
                          @NotNull GameSessionService gameSessionService,
                          @NotNull AccountService accountService,
                          @NotNull RemotePointService remotePointService) {
         this.clientSnapshotsService = clientSnapshotsService;
-        this.serverSnapshotService = serverSnapshotService;
         this.gameSessionService = gameSessionService;
         this.accountService = accountService;
         this.remotePointService = remotePointService;
@@ -63,7 +58,7 @@ public class GameMechanics {
         if (user != null) {
             if (mode.equals(Config.SINGLE_MODE)) {
                 singleWaiters.add(user);
-            } else {
+            } else if (mode.equals(Config.MULTI_MODE)) {
                 multiWaiters.add(user);
             }
             if (LOGGER.isDebugEnabled()) {
