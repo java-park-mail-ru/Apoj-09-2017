559a666c70190c7eb1587f10948838590e6aa666
ilyamoskalev <ilyamoskalev@mail.ru>
2017-12-07 00:45:17 +0300

    working singleplayer

--- src/main/java/application/mechanic/Config.java
+++ src/main/java/application/mechanic/Config.java
@@ -1,8 +1,8 @@
 package application.mechanic;
 
 public class Config {
-    public static final String SINGLE_MODE = "Singleplayer";
-    public static final String MULTI_MODE = "Multiplayer";
+    public static final String SINGLE_MODE = "singleplayer";
+    public static final String MULTI_MODE = "multiplayer";
     public static final String SINGER_ROLE = "singer";
     public static final String LISTENER_ROLE = "listener";
     public static final String STEP_0 = "preGameData";
