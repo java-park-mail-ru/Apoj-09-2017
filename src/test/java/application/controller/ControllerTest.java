3bbf81d5228ba5ea5b39a73a2e79853f277a52ac
ilyamoskalev <ilyamoskalev@mail.ru>
2017-12-18 18:21:56 +0300

    ControllerTest RANDOM_PORT -> MOCK

--- src/test/java/application/controller/ControllerTest.java
+++ src/test/java/application/controller/ControllerTest.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc(print = MockMvcPrint.NONE)
 public class ControllerTest {
     private static final String LOGIN = "ilya";
