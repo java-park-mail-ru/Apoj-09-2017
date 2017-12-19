20fb985ba25fe2525c69c76f60ba0ac2d9b46f90
ilyamoskalev <ilyamoskalev@mail.ru>
2017-12-19 22:26:07 +0300

    delete unused imports

--- src/test/java/application/utils/ValidatorTest.java
+++ src/test/java/application/utils/ValidatorTest.java
@@ -2,9 +2,6 @@ package application.utils;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 public class ValidatorTest {
     @Test
